476b21064ddcf2c1ea43d5e26ba4b440ea879508
Yuval Nahmias <yuvalit789@gmail.com>
2023-11-26 15:56:28 -0500

    itinial commit

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={8a7f98a4a4a793399f951c1d433d5a22
+}
+
+
+fetch (https:api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={8a7f98a4a4a793399f951c1d433d5a22
+})
