580db1e3ac84217677c56714c586de2171b968bf
Yuval Nahmias <yuvalit789@gmail.com>
2024-02-01 14:13:41 -0500

    working api fetch, broken loop for 5 day weather forcast

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # WeatherForcast
+
+A forcast webpage, presetnting the
+
+## User Story
+
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,49 @@
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+var searchBtn = document.getElementById("searchBtn");
+var APIkey = "8a7f98a4a4a793399f951c1d433d5a22";
+// var city = "london";
+var dailyTemp = document.getElementById("dailyTemp");
+let cityEl = document.getElementById("cityName");
+let dateEl = document.getElementById("dateEl");
+let currentTempEl = document.getElementById("currentTemp");
+let currentWindEl = document.getElementById("currentWind");
+let currentHumEl = document.getElementById("currentHum");
 
-https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={8a7f98a4a4a793399f951c1d433d5a22
+function getApi() {
+  var city = document.getElementById("search-form").value;
+  var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIkey}`;
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      getWeather(lat, lon);
+    });
 }
+searchBtn.addEventListener("click", getApi);
 
-
-fetch (https:api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={8a7f98a4a4a793399f951c1d433d5a22
-})
+function getWeather(lat, lon) {
+  var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}&units=metric`;
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var temp = data.list[0].main.temp;
+      var currentDate = data.list[0].dt_txt;
+      var currentWind = data.list[0].wind.speed;
+      var currentHum = data.list[0].main.humidity;
+      cityEl.textContent = document.getElementById("search-form").value;
+      dateEl.textContent = currentDate;
+      currentTempEl.textContent = `${temp} c°`;
+      currentWindEl.textContent = currentWind;
+      currentHumEl.textContent = `${currentHum}%`;
+      for (var i = 0; i < 6; i += 8) {
+        var nextTemp = data.list[i].main.temp;
+        console.log(nextTemp);
+      }
+    });
+}
